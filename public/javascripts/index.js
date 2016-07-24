9c7bb0670ce0ec3e8caba6852ef39d6006cfe299
Ryan <ryan.o.mackey@gmail.com>
2016-07-23 18:38:36 -0600

    styling changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var open = true;
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -10,7 +12,10 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-var open = true;
+if (window.screen.width < 450) {
+  closeNav();
+  open = false;
+}
 
 document.getElementById("navToggle").addEventListener('click', function() {
   if (open) {
